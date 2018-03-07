8858cd1cfc6d4f399c396807999cedffd7250c0b
Oliver Nybroe <olivernybroe@gmail.com>
2018-03-07 11:42:22 +0100

    fight club

---
+++ videoStream.py
@@ -0,0 +1,49 @@
+import cv2
+from time import sleep
+
+
+def _try():
+    running = False
+    while not running:
+        print("Trying to connect.")
+        cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+        running = cam.isOpened()
+        sleep(0.1)
+
+    while running:
+        running, frame = cam.read()
+        if running & frame is not None:
+            _process_image(frame)
+
+    print("Disconnected, reconnecting to drone.")
+    _try()
+
+
+def _process_image(image):
+    cv2.imshow('frame', image)
+    if cv2.waitKey(1) & 0xFF == 27:
+        exit(0)
+
+
+_try()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+'''
+
+running = True
+while running:
+    # get current frame of video
+    running, frame = cam.read()
+    if running:
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            # escape key pressed
+            running = False
+    else:
+        # error reading frame
+        print('error reading video feed')
+cam.release()
+cv2.destroyAllWindows()
+'''
