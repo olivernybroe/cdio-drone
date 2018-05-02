935a7f7bbf898bf8edeb048ebd5d346e75f01ed1
Oliver Nybroe <olivernybroe@gmail.com>
2018-05-02 10:41:16 +0200

    Added more standardized format for recognition and docs

--- Recognition/Recognition.py
+++ Recognition/Recognition.py
@@ -6,10 +6,22 @@ class Recognition:
     image = None
 
     def __init__(self, image):
+        """
+        Construct the recognition object.
+
+        When using the recognition supply it a image and later call recognize
+        and it will return with the results of where it has found QR codes and
+        where it found rings.
+
+        :param image:
+        """
         self.image = image
 
     def recognize(self):
-        return self._find_qr()
+        return {
+            'qr': self._find_qr(),
+            'rings': []
+        }
 
     def _find_qr(self):
         return Qr.recognize(self.image)
